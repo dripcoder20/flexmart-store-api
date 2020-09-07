e246ccea39122a339e39e0d43d1573fd42875a0f
Kristher Vidal <kristher@dripcoder.com>
2020-09-07 20:19:39 +0800

    187 - Refactor Cart Endpoint

--- tests/Feature/controllers/CartControllerTest.php
+++ tests/Feature/controllers/CartControllerTest.php
@@ -28,15 +28,15 @@ class CartControllerTest extends TestCase
     /** @test */
     public function it_should_list_all_the_items_in_the_cart()
     {
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
         $this->product['product_id'] = 2;
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
-        $this->post('/api/cart/2', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/2/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
         $this->withoutExceptionHandling();
-        $response  = $this->get('/api/cart/1');
+        $response  = $this->get('/api/1/cart');
         $response->assertStatus(200);
         $this->assertCount(2, $response->json()['data']);
-        $response  = $this->get('/api/cart/2');
+        $response  = $this->get('/api/2/cart');
         $response->assertStatus(200);
         $this->assertCount(1, $response->json()['data']);
     }
@@ -44,20 +44,20 @@ class CartControllerTest extends TestCase
     /** @test */
     public function it_should_add_item_to_cart()
     {
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
         $this->assertDatabaseHas('carts', ['user_id' => 1, 'product_id' => $this->product['product_id']]);
         $this->assertDatabaseCount('carts', 1);
         $this->product['product_id'] = 2;
-        $this->post('/api/cart/1', $this->product + ['quantity' => 2])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 2])->assertStatus(Response::HTTP_ACCEPTED);
         $this->assertDatabaseCount('carts', 2);
     }
 
     /** @test */
     public function it_should_update_cart_item()
     {
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
-        $this->put('/api/cart/1', ['product_id' => $this->product['product_id'], 'quantity' => 2])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->put('/api/1/cart', ['product_id' => $this->product['product_id'], 'quantity' => 2])->assertStatus(Response::HTTP_ACCEPTED);
         $this->assertDatabaseHas('carts', ['user_id' => 1, 'product_id' => $this->product['product_id'], 'quantity' => 2]);
         $this->assertDatabaseCount('carts', 1);
     }
@@ -65,8 +65,8 @@ class CartControllerTest extends TestCase
     /** @test */
     public function it_should_remove_item_from_cart()
     {
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
-        $this->delete('/api/cart/1', ['product_id' => $this->product['product_id']])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->delete('/api/1/cart', ['product_id' => $this->product['product_id']])->assertStatus(Response::HTTP_ACCEPTED);
         $this->assertDatabaseCount('carts', 0);
     }
 
@@ -74,11 +74,11 @@ class CartControllerTest extends TestCase
     public function it_should_remove_multiple_items_from_cart()
     {
         $this->withoutExceptionHandling();
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
         $this->product['product_id'] = 2;
-        $this->post('/api/cart/1', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->post('/api/1/cart', $this->product + ['quantity' => 1])->assertStatus(Response::HTTP_ACCEPTED);
         $this->product['product_id'] = 3;
-        $this->delete('/api/cart/1', ['product_id' => [1, 2, 3]])->assertStatus(Response::HTTP_ACCEPTED);
+        $this->delete('/api/1/cart', ['product_id' => [1, 2, 3]])->assertStatus(Response::HTTP_ACCEPTED);
         $this->assertDatabaseCount('carts', 0);
     }
 }
